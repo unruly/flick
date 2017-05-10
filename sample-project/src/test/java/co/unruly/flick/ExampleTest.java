9fd4ce5a644535ef53979f537da2b0639debed89
Alex Wilson <a.wilson@alumni.warwick.ac.uk>
2017-05-10 16:17:39 +0100

    Extract sample project
    - PhantomJSTest is now ExampleTest under sample-project
    - Clean up dependencies

---
+++ sample-project/src/test/java/co/unruly/flick/ExampleTest.java
@@ -0,0 +1,64 @@
+package co.unruly.flick;
+
+import co.unruly.flick.browser.Browser;
+import co.unruly.flick.browser.Page;
+import co.unruly.flick.dsl.Action;
+import co.unruly.flick.dsl.Actor;
+import co.unruly.flick.dsl.Assertion;
+import org.junit.After;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+public class ExampleTest implements FlickDSL {
+
+    private static class User implements Actor {}
+
+    private final Browser browser = new PhantomJSBrowser();
+
+    @Test
+    public void shouldLoadPage() {
+        Actor actor = new User();
+
+        Page page = given(actor.using(browser, "http://example.com"))
+                .hasHappened();
+
+        then(actor.using(browser, page))
+            .should(beAbleToSeeHeader());
+    }
+
+    @Test
+    public void shouldNavigateToMoreInfo() {
+        Actor actor = new User();
+
+        Page page = given(actor.using(browser, "http://example.com"))
+                .wasAbleTo(getMoreInfo())
+                .hasHappened();
+
+        then(actor.using(browser, page))
+            .should(beOnPage("https://www.iana.org/domains/reserved"));
+    }
+
+    @After
+    public void tearDown() {
+        browser.shutdown();
+    }
+
+    private Action getMoreInfo() {
+        return (browser) -> browser.findElement(By.xpath("/html/body/div/p[2]/a")).click();
+    }
+
+    private Assertion beOnPage(String expectedUrl) {
+        return (browser) -> browser.waitUntilEquals(
+            () -> browser.getDriver().getCurrentUrl(),
+            expectedUrl
+        );
+    }
+
+    private Assertion beAbleToSeeHeader() {
+        return (browser) -> browser.waitUntilEquals(
+            () -> browser.findElement(By.xpath("/html/body/div/h1")).getText(),
+            "Example Domain"
+        );
+    }
+
+}
